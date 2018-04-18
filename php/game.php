971baca8d2856906b58490cdd43756af8f29ee8e
Marco De Nicolo <maerkdeni@gmail.com>
2018-04-18 22:41:49 +0200

    Added copyright

--- php/game.php
+++ php/game.php
@@ -1,7 +1,10 @@
 <?php
 /* This Source Code Form is subject to the terms of the Mozilla Public
 * License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+* file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*
+*        Copyright 2018 Marco De Nicolo
+*/
 
 if(!isset($_SESSION['logged']) || $_SESSION['logged'] == 0)
     header('Location: index.php');
